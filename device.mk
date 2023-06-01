309c4a3c0830e38d60264590a1c369407869796d
AdarshGrewal <adarshgrewal@gmail.com>
2023-06-01 14:01:56 +0530

    munch: rro_overlays: Set correct device name and hotspot SSID
    Check device variant by using ro.boot.hwc and set the correct
    device name based on value of ro.product.marketname on stock ROM
    
    Change-Id: Iae15a7d3558f3c4608546e42ed655a76a0fe8868

--- device.mk
+++ device.mk
@@ -15,6 +15,14 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_PACKAGES += \
+    MunchCNSettingsProviderOverlay \
+    MunchCNWifiOverlay \
+    MunchGLOBALSettingsProviderOverlay \
+    MunchGLOBALWifiOverlay \
+    MunchINDIASettingsProviderOverlay \
+    MunchINDIAWifiOverlay \
+
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
